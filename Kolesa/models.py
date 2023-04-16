29afc5151c01fe5137a28712b3d4b0a36ce57d91
bekzat <bekzat.kalau@narxoz.kz>
2023-04-16 20:42:58 +0600

    database commit

--- Kolesa/models.py
+++ Kolesa/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Kolesa(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(update_to="photos/%Y%m/%d")
+    time_create = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+
